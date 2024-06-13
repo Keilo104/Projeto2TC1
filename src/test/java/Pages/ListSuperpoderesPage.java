6b5fadbb503bf983eb7b57c13fc6222cf978c108
keilo <keilogamimg@gmail.com>
2024-06-13 16:38:37 -0300

    feat: initial implementation of ListSuperpoderesPage

--- src/test/java/Pages/ListSuperpoderesPage.java
+++ src/test/java/Pages/ListSuperpoderesPage.java
@@ -1,4 +1,22 @@
 package Pages;
 
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+
 public class ListSuperpoderesPage {
+    protected WebDriver driver;
+
+    private final By returnToHomePageLink = By.xpath("/html/body/div/body/div/header/h1/a");
+    private final By goToCadastrarPageLink = By.xpath("/html/body/div/body/div/header/nav/ul/li/a");
+
+    private final By allSuperpoderes = By.className("post");
+
+    public ListSuperpoderesPage(WebDriver driver) {
+        this.driver = driver;
+
+        if(!driver.getCurrentUrl().equals("https://site-tc1.vercel.app/")) {
+            throw new IllegalStateException("This is not the List Superpoderes Inúteis page," +
+                    "current page is: " + driver.getCurrentUrl());
+        }
+    }
 }
