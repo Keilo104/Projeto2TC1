335739b386667d067815937e4cb9f212f9ee99c3
keilo <keilogamimg@gmail.com>
2024-06-13 16:56:39 -0300

    test: initial implementation of shouldSuccessfullyCreateSuperpowerFromFakerAndCheck in CadastroSuperpoderesTest, but without Faker

--- src/test/java/SuperpoderTest/CadastroSuperpoderesTest.java
+++ src/test/java/SuperpoderTest/CadastroSuperpoderesTest.java
@@ -1,13 +1,18 @@
 package SuperpoderTest;
 
+import Model.Superpoder;
 import Pages.CadastroSuperpoderPage;
+import Pages.ListSuperpoderesPage;
 import io.github.bonigarcia.wdm.WebDriverManager;
+import org.assertj.core.api.SoftAssertions;
 import org.junit.jupiter.api.*;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
 import java.time.Duration;
+import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.assertEquals;
@@ -53,6 +58,39 @@ public class CadastroSuperpoderesTest {
             page.goToCadastro();
 
             assertEquals("https://site-tc1.vercel.app/cadastro", driver.getCurrentUrl());
+        }
+    }
+
+    @Nested
+    @DisplayName("Tests for creating superpoderes")
+    class CreateSuperpoderes {
+        @Test
+        @DisplayName("Should be able to successfully create a superpower from faker and check if it's been properly created")
+        void shouldSuccessfullyCreateSuperpowerFromFakerAndCheck() {
+            final SoftAssertions softly = new SoftAssertions();
+
+            Superpoder superpoder = new Superpoder();
+
+            superpoder.setNome("Poder inútil 1");
+            superpoder.setDescricao("Faz coisas inúteis");
+            superpoder.setEfeitosColaterais("Péssimos efeitos");
+            superpoder.setNota("3");
+
+            page.createSuperpoderFromSuperpoder(superpoder);
+
+            webDriverWait.until(ExpectedConditions.alertIsPresent());
+            String alertMessage = driver.switchTo().alert().getText();
+            driver.switchTo().alert().accept();
+
+            page.returnToHomePage();
+
+            ListSuperpoderesPage listSuperpoderesPage = new ListSuperpoderesPage(driver);
+            List<Superpoder> superpoderesList = listSuperpoderesPage.getSuperpoderes();
+
+            softly.assertThat(alertMessage).isEqualTo("Poder criado com sucesso!");
+            softly.assertThat(superpoderesList).contains(superpoder);
+
+            softly.assertAll();
         }
     }
 }
