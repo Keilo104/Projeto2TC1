14bfa84b596e5e3ca1d17962c7dff2f6f577307f
keilo <keilogamimg@gmail.com>
2024-06-13 16:44:54 -0300

    feat: finishing implementation of getSuperpoderes by implementing a constructor from WebElement for Superpoder

--- src/test/java/Model/Superpoder.java
+++ src/test/java/Model/Superpoder.java
@@ -1,6 +1,17 @@
 package Model;
 
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+
 public class Superpoder {
+    private final By nomeDoPoderElement = By.className("post-title");
+    private final By descricaoElement = By.className("post-excerpt");
+    private final By efeitosColateraisElement = By.xpath("//p[2]");
+    private final By notaSelectElement = By.className("stars");
+
+    private final By editButton = By.xpath("//div/button[1]");
+    private final By deleteButton = By.xpath("//div/button[2]");
+
     private String nome;
     private String descricao;
     private String efeitosColaterais;
@@ -8,12 +19,23 @@ public class Superpoder {
 
     public Superpoder() { }
 
+    public Superpoder(WebElement e) {
+        this.setNomeFromElement(e);
+        this.setDescricaoFromElement(e);
+        this.setEfeitosColateraisFromElement(e);
+        this.setNotaFromElement(e);
+    }
+
     public String getNome() {
         return nome;
     }
 
     public void setNome(String nome) {
         this.nome = nome;
+    }
+
+    private void setNomeFromElement(WebElement e) {
+        this.nome = e.findElement(nomeDoPoderElement).getText();
     }
 
     public String getDescricao() {
@@ -24,6 +46,10 @@ public class Superpoder {
         this.descricao = descricao;
     }
 
+    private void setDescricaoFromElement(WebElement e) {
+        this.descricao = e.findElement(descricaoElement).getText();
+    }
+
     public String getEfeitosColaterais() {
         return efeitosColaterais;
     }
@@ -32,11 +58,23 @@ public class Superpoder {
         this.efeitosColaterais = efeitosColaterais;
     }
 
+    private void setEfeitosColateraisFromElement(WebElement e) {
+        String efeitosColaterais = e.findElement(efeitosColateraisElement).getText();
+
+        this.efeitosColaterais = efeitosColaterais.substring(20);
+    }
+
     public String getNota() {
         return nota;
     }
 
     public void setNota(String nota) {
         this.nota = nota;
+    }
+
+    private void setNotaFromElement(WebElement e) {
+        String stars = e.findElement(notaSelectElement).getText();
+
+        this.nota = Integer.toString(stars.length());
     }
 }
