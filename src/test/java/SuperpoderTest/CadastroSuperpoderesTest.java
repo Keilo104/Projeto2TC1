96b0a2f0f2e804b595b2889f2326fbf92ae4ea4d
keilo <keilogamimg@gmail.com>
2024-06-13 16:34:16 -0300

    test: implementation of shouldPathToHome test in CadastroSuperpoderesTest

--- src/test/java/SuperpoderTest/CadastroSuperpoderesTest.java
+++ src/test/java/SuperpoderTest/CadastroSuperpoderesTest.java
@@ -9,6 +9,9 @@ import org.openqa.selenium.support.ui.WebDriverWait;
 
 import java.time.Duration;
 
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 public class CadastroSuperpoderesTest {
     private WebDriver driver;
     private WebDriverWait webDriverWait;
@@ -31,4 +34,18 @@ public class CadastroSuperpoderesTest {
     void destroy() {
         driver.quit();
     }
+
+    @Nested
+    @DisplayName("Tests for pathing to other places")
+    class Pathing {
+
+        @Test
+        @DisplayName("Should path to home when clicking home link")
+        void shouldPathToHome() {
+            page.returnToHomePage();
+
+            assertEquals("https://site-tc1.vercel.app/", driver.getCurrentUrl());
+        }
+    }
+
 }
