473b6bb63ab6ecab9f75f7610f894f2581b834ad
AlexandreSCavaleiro <alexandre_cavaleiro@outlook.com>
2024-06-13 17:34:43 -0300

    feat: begginig to build the faker, with getValidDescricao and getValidName

--- src/test/java/Faker/SuperpoderFakerUtil.java
+++ src/test/java/Faker/SuperpoderFakerUtil.java
@@ -1,4 +1,31 @@
 package Faker;
 
+import com.github.javafaker.Faker;
+import org.apache.commons.lang3.StringUtils;
+
+import java.util.Random;
+
 public class SuperpoderFakerUtil {
+    private static final Faker faker = new Faker();
+    private static final Random random = new Random();
+
+    public static String getValidNome(){
+        return String.format("%s %s %s",
+                faker.resolve("games.pokemon.moves"),
+                StringUtils.capitalize(faker.resolve("lovecraft.words")),
+                faker.superhero().suffix()
+        );
+    }
+
+    public static String getValidDescricao(){
+        String control = random.nextBoolean() ? "" : " no";
+
+        return String.format("User is able to %s, but only %d times every %d months, and they have %s control over it.",
+                faker.superhero().power(),
+                random.nextInt(6)+2,
+                random.nextInt(16)+3,
+                control
+        );
+    }
+
 }
