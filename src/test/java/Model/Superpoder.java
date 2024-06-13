aa075634705c7ef685a5c96cec2efe269a6c2bd6
keilo <keilogamimg@gmail.com>
2024-06-13 16:39:08 -0300

    feat: initial implementation of Superpoder

--- src/test/java/Model/Superpoder.java
+++ src/test/java/Model/Superpoder.java
@@ -1,4 +1,42 @@
 package Model;
 
 public class Superpoder {
+    private String nome;
+    private String descricao;
+    private String efeitosColaterais;
+    private String nota;
+
+    public Superpoder() { }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
+
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
+    }
+
+    public String getEfeitosColaterais() {
+        return efeitosColaterais;
+    }
+
+    public void setEfeitosColaterais(String efeitosColaterais) {
+        this.efeitosColaterais = efeitosColaterais;
+    }
+
+    public String getNota() {
+        return nota;
+    }
+
+    public void setNota(String nota) {
+        this.nota = nota;
+    }
 }
