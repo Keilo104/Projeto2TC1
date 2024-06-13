db876e2c36b7cad8b1588b84f4073b269ad3080f
keilo <keilogamimg@gmail.com>
2024-06-13 16:32:52 -0300

    test: initial implementation of CadastroSuperpoderesTest, with setup and destroy functions

--- src/test/java/SuperpoderTest/CadastroSuperpoderesTest.java
+++ src/test/java/SuperpoderTest/CadastroSuperpoderesTest.java
@@ -1,4 +1,34 @@
 package SuperpoderTest;
 
+import Pages.CadastroSuperpoderPage;
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.junit.jupiter.api.*;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.time.Duration;
+
 public class CadastroSuperpoderesTest {
+    private WebDriver driver;
+    private WebDriverWait webDriverWait;
+
+    private final String PAGE_URL = "https://site-tc1.vercel.app/cadastro";
+    private CadastroSuperpoderPage page;
+
+    @BeforeEach
+    void setup() {
+        WebDriverManager.firefoxdriver().setup();
+
+        driver = new FirefoxDriver();
+        driver.get(PAGE_URL);
+
+        page = new CadastroSuperpoderPage(driver);
+        webDriverWait = new WebDriverWait(driver, Duration.ofSeconds(10));
+    }
+
+    @AfterEach
+    void destroy() {
+        driver.quit();
+    }
 }
