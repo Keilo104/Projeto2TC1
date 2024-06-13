34b8d0e77b6c7b572033f94c38ad7bfd74091f58
keilo <keilogamimg@gmail.com>
2024-06-13 16:46:14 -0300

    test: initial implementation of ListSuperpoderesTest with setup and destroy functions

--- src/test/java/SuperpoderTest/ListSuperpoderesTest.java
+++ src/test/java/SuperpoderTest/ListSuperpoderesTest.java
@@ -1,4 +1,34 @@
 package SuperpoderTest;
 
+import Pages.ListSuperpoderesPage;
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.junit.jupiter.api.*;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.time.Duration;
+
 public class ListSuperpoderesTest {
+    private WebDriver driver;
+    private WebDriverWait webDriverWait;
+
+    private final String PAGE_URL = "https://site-tc1.vercel.app/";
+    private ListSuperpoderesPage page;
+
+    @BeforeEach
+    void setup() {
+        WebDriverManager.firefoxdriver().setup();
+
+        driver = new FirefoxDriver();
+        driver.get(PAGE_URL);
+
+        page = new ListSuperpoderesPage(driver);
+        webDriverWait = new WebDriverWait(driver, Duration.ofSeconds(10));
+    }
+
+    @AfterEach
+    void destroy() {
+        driver.quit();
+    }
 }
